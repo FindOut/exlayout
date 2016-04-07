758374a2d93f72c208128875133ca0e3ede960ca
huan Xie <huan Xie>
2016-04-07 16:29:09 +0200

    Connected Graph Detection and test finished

---
+++ spec/jasmine_test/SugiyamaTestSpec.js
@@ -0,0 +1,23 @@
+describe("crossing_reducing_rate", function(){
+  it("Test rate of reduced crossing", function(){
+    var graph = {
+      "nodes": [
+        {"id": 1, "label": "A", "rank": 0, "isDummy": false},
+        {"id": 2, "label": "B", "rank": 0, "isDummy": false},
+        {"id": 3, "label": "C", "rank": 0, "isDummy": false},
+        {"id": 4, "label": "D", "rank": 0, "isDummy": false},
+        {"id": 5, "label": "E", "rank": 0, "isDummy": false},
+        {"id": 6, "label": "F", "rank": 0, "isDummy": false},
+        {"id": 7, "label": "G", "rank": 0, "isDummy": false}
+      ],
+      "links": [
+        {"from": 1, "to": 2},
+        {"from": 2, "to": 3},
+        {"from": 2, "to": 5},
+        {"from": 2, "to": 4},
+        {"from": 3, "to": 5},
+        {"from": 4, "to": 5},
+        {"from": 5, "to": 6},
+        {"from": 7, "to": 4}
+      ]
+    };
